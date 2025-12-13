c43532dd5ec7ea87654847e74bf820703a138e56
Karo Rios <hkrios10@ucatolica.edu.co>
2025-12-13 10:02:28 -0500

    creacion carpeta app y main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
-import uvicorn
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    uvicorn.run(
-        "app:app",
-        host="127.0.0.1",
-        port=8000,
-        reload=True
-    )
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"status": "ok"}
